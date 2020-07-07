d8cacc560ac7f91423b214a1f6ba9431ac7b8a0f
Arnaud Mary <arnaud.mary@gmail.com>
2020-07-07 15:12:26 +0200

    Release 0.0.1

--- src/compile/Compiler.js
+++ src/compile/Compiler.js
@@ -9,40 +9,7 @@ class Compiler {
   }
 
   compileBuffer(buffer) {
-    // this.compileBufferFastest(buffer);
-    this.compileBufferSlow(buffer);
-  }
 
-  compileBufferFastest(buffer) {
-    buffer.forEach(block => {
-      if (block.type === 'r') {
-        // reference
-        this.r += `r+=l.${block.tag};`;
-      } else if (block.type === '?' || block.type === '^' ) {
-        // conditional block
-        this.r += `if(${block.type === '^' ? '!' : ''}(l.${block.tag})){`;
-        this.compileBuffer(block.buffer);
-        this.r += '}';
-        // else
-        if (block.bodies && block.bodies.else) {
-          this.r += 'else {';
-          this.compileBuffer(block.bodies.else);
-          this.r += '}';
-        }
-      } else if (block.type === '#') {
-        // loop block
-        this.r += `for(var i=0;i<l.${block.tag}.length;i++){`;
-        this.r += `l.it=l.${block.tag}[i];`;
-        this.compileBuffer(block.buffer);
-        this.r += '}';
-      } else if (!block.type){
-        // default: raw text
-        this.r += `r+='${block}';`;
-      }
-    });
-  }
-
-  compileBufferSlow(buffer) {
     buffer.forEach(block => {
       if (block.type === 'r') {
         this.r += `l.it=ctx.stack[ctx.stack.length - 1];`;
