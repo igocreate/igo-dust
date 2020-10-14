9ddfbbe73fe33836ed5fed2f54450ec9fe4a4531
Benoit <benoit.sourd@igocreate.com>
2020-10-14 17:23:39 +0200

    Add locals to web testing

--- public/main.js
+++ public/main.js
@@ -2,10 +2,24 @@
 
 
 var template = document.getElementById('template');
+var locals = document.getElementById('locals');
 var button = document.getElementById('compile');
 var result = document.getElementById('result');
 
 document.getElementById('compile').onclick = function() {
   var compiled  = IgoDust.compile(template.value);
-  result.innerHTML = IgoDust.render(compiled, {w: 'world'});
+
+  var data = locals.value;
+  if (!data) {
+    result.innerHTML = IgoDust.render(compiled);
+    return 
+  }
+
+  try {
+    const f = new Function('return ' + data + ';');
+    data = f();
+    result.innerHTML = IgoDust.render(compiled, data);
+  } catch {
+    result.innerHTML = 'Invalid data :('
+  }
 };
