db8afdc6f1a168234baa8867c47e4fb15fe662fe
Anatolij Ostroumov <ostroumov095@gmail.com>
2020-11-18 15:39:46 +0300

    npm run build generates build artifacts following pattern including version from package.json - `igo-dust-0.1.14-min.js` and also copies it to documentation sample

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const environment = require('./src/environment');
-const Parser    = require('./src/parse/Parser');
-const Compiler  = require('./src/compile/Compiler');
-const Renderer  = require('./src/render/Renderer');
-const Helpers   = require('./src/render/Helpers');
-const Utils     = require('./src/render/Utils');
+const Parser = require('./src/parse/Parser');
+const Compiler = require('./src/compile/Compiler');
+const Renderer = require('./src/render/Renderer');
+const Helpers = require('./src/render/Helpers');
+const Utils = require('./src/render/Utils');
 
-const Cache     = require('./src/Cache');
-const config    = require('./src/Config');
+const Cache = require('./src/Cache');
+const config = require('./src/Config');
 
 //
 module.exports.compile = (src) => {
-  const buffer  = new Parser().parse(src);
+  const buffer = new Parser().parse(src);
   return new Compiler().compile(buffer);
 };
 
@@ -34,14 +34,18 @@ module.exports.engine = (filePath, options, callback) => {
 };
 
 //
-module.exports.helpers  = Helpers;
-module.exports.filters  = Utils.f;
-module.exports.config   = config;
+module.exports.helpers = Helpers;
+module.exports.filters = Utils.f;
+module.exports.config = config;
 
 if (environment.isBrowser) {
   window.IgoDust = {
-    render:  (compiled, data) => {
+    render: (compiled, data) => {
       return new Renderer().render(compiled, data);
-    }
+    },
+    compile: (src) => {
+      const buffer = new Parser().parse(src);
+      return new Compiler().compile(buffer);
+    },
   };
 }
