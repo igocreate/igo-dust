d96cd9afb35548717f2870aceb4284dbcb6a86de
Anatolij Ostroumov <ostroumov095@gmail.com>
2020-11-18 15:21:18 +0300

    npm run build generates build artifacts following pattern including version from package.json - `igo-dust-0.1.14-min.js` and so on

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const path = require('path');
+const version = require('./package.json').version;
 
 module.exports = exports = {
   entry: path.join(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'igo-dust.bundle.js'
+    filename: `igo-dust-${version}-min.js`
   },
   resolve: {
     fallback: { path: false, fs: false}
