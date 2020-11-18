e609be5c1955e29068fa829367c6aee824403ebe
Anatolij Ostroumov <ostroumov095@gmail.com>
2020-11-18 15:39:46 +0300

    npm run build generates build artifacts following pattern including version from package.json - `igo-dust-0.1.14-min.js` and also copies it to documentation sample

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,30 @@
 
 const path = require('path');
 const version = require('./package.json').version;
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
 
 module.exports = exports = {
   entry: path.join(__dirname, 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: `igo-dust-${version}-min.js`
   },
   resolve: {
-    fallback: { path: false, fs: false}
-  }
+    fallback: {path: false, fs: false}
+  },
+  plugins: [
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            {
+              source: path.join(__dirname, 'dist', `igo-dust-${version}-min.js`),
+              destination: path.join(__dirname, 'docs', 'js', 'bundle.js')
+            }
+          ]
+        }
+      }
+    })
+  ]
 };
