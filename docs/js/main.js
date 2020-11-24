f0533b0ccce41f31c11af005ed33bd366ea1dc3c
Anatolij Ostroumov <ostroumov095@gmail.com>
2020-11-24 16:47:31 +0300

    working on desight

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,37 +2,28 @@
 
 /* global IgoDust */
 
-const template = document.getElementById('template');
-const locals = document.getElementById('locals');
-const button = document.getElementById('compile');
-const result = document.getElementById('result');
-
-const initial =
-    'Hello {w}!'+
-    '<br/><br/>'+
-    'My friends are {#friends}{@last} and {/last}{.}{@sep}, {/sep}{/friends}.'+
-    '<br/>'+
-    '{?hasFriends}Yay friends!{/hasFriends}';
-
-window.onload = function () {
-  template.value = initial;
+function init(tpl, data, document) {
+  const template = document.getElementById('template');
+  const locals = document.getElementById('locals');
+  const button = document.getElementById('compile');
+  const result = document.getElementById('result');
+  template.value = tpl;
+  locals.value = data;
+  button.onclick = function() {
+    const compiled  = IgoDust.compile(template.value);
+    let data = locals.value;
+    if (!data) {
+      result.innerHTML = IgoDust.render(compiled);
+      return;
+    }
+    try {
+      const f = new Function('return ' + data + ';');
+      data = f();
+      result.innerHTML = IgoDust.render(compiled, data);
+    } catch (e) {
+      result.innerHTML = `Error while compiling - ${e.message}`;
+      console.error(e); // eslint-disable-line
+    }
+    return false;
+  };
 }
-
-button.onclick = function() {
-  const compiled  = IgoDust.compile(template.value);
-  let data = locals.value;
-  if (!data) {
-    result.innerHTML = IgoDust.render(compiled);
-    return;
-  }
-
-  try {
-    const f = new Function('return ' + data + ';');
-    data = f();
-    result.innerHTML = IgoDust.render(compiled, data);
-  } catch (e) {
-    result.innerHTML = `Error while compiling - ${e.message}`;
-    console.error(e); // eslint-disable-line
-  }
-  return false;
-};
