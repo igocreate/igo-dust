398720b500c42f26691cd8e30f2a3af25d16bfb6
Arnaud Mary <arnaud@MacBook-Pro-de-Arnaud-M3.local>
2025-10-08 17:57:26 +0200

    simplify config

--- src/Config.js
+++ src/Config.js
@@ -11,13 +11,16 @@ class Config {
 
   configure(settings) {
 
-    if (settings['view cache'] !== undefined) {
-      this.cache = !!settings['view cache'];
-    }
     if (settings.views !== undefined) {
       this.views = settings.views;
     }
-    ['htmlencode', 'htmltrim', 'cache'].forEach((key) => {
+
+    // Handle Express 'view cache' setting (cache takes priority)
+    if (settings.cache !== undefined || settings['view cache'] !== undefined) {
+      this.cache = !!(settings.cache ?? settings['view cache']);
+    }
+
+    ['htmlencode', 'htmltrim'].forEach((key) => {
       if (settings[key] !== undefined) {
         this[key] = !!settings[key];
       }
